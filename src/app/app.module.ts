18d72bc724f942adcab9dab45b927d85d15fa90b
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-09-02 22:11:57 +0530

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UploadComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

38ad8ebb4977496556858f6686a066da41cf9383
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-09-03 00:09:09 +0530

    user name setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    if (!localStorage.getItem('user')) {
+      localStorage.setItem('user',
+        Date.now().toString()
+      )
+    }
   }
 
 }
